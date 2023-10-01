dd6d380c1014fb6b62949484a954444c34855be2
Sumit Pandey <spandey247789@gmail.com>
2023-10-01 19:50:35 +0530

    JSON Exercise

---
+++ JSONExercise.ts
@@ -0,0 +1,24 @@
+type JSONObject = { [key: string]: JSONValue };
+type JSONArray = JSONValue[];
+type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
+
+function isJSON(arg: JSONValue) {}
+
+// POSITIVE test cases (must pass)
+isJSON("hello");
+isJSON([4, 8, 15, 16, 23, 42]);
+isJSON({ greeting: "hello" });
+isJSON(false);
+isJSON(true);
+isJSON(null);
+isJSON({ a: { b: [2, 3, "foo"] } });
+
+// NEGATIVE test cases (must fail)
+// @ts-expect-error
+isJSON(() => "");
+// @ts-expect-error
+isJSON(class {});
+// @ts-expect-error
+isJSON(undefined);
+// @ts-expect-error
+isJSON(isJSON);
